3ffbb9c0e4e35f623259413cb11f36eeb4a6daef
Jaiteg Chahal <jchahal@berkeley.edu>
2023-07-08 16:53:22 -0700

    Updated with comments." "

--- chatGPT.py
+++ chatGPT.py
@@ -1,23 +1,20 @@
 import os
 import sys
-import math
-#import constants
-import time
 import pandas as pd
 import requests
-
 from langchain.document_loaders import TextLoader
 from langchain.document_loaders import DirectoryLoader
 from langchain.indexes import VectorstoreIndexCreator
 from langchain.chat_models import ChatOpenAI
-
-import streamlit as st
 from langchain.llms import OpenAI
-from langchain.prompts import PromptTemplate
+import streamlit as st
 
 
-
+# Gets OPEN_AI API key.
 os.environ["OPENAI_API_KEY"] = st.secrets["openai_secret_key"]
+
+# Add the title            
+st.title('🏀 NBA GPT | 2022-2023 NBA Season')
 
 # Display the text in the upper corner
 st.markdown(
@@ -29,46 +26,33 @@ st.markdown(
     unsafe_allow_html=True
 )
 
+# Add information sidebar.
 st.sidebar.text("Jaiteg Chahal")
-
 st.sidebar.markdown('[LinkedIn](https://www.linkedin.com/in/jaiteg-chahal/)', unsafe_allow_html=True)
 st.sidebar.markdown('[Github](https://github.com/jaitegchahal123)', unsafe_allow_html=True)
 st.sidebar.text("jchahal@berkeley.edu")
+
 # Add a horizontal line using HTML tags
 st.sidebar.markdown("<br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>", unsafe_allow_html=True)
-
 st.sidebar.markdown("<hr>", unsafe_allow_html=True)
 
-# Add the "published" text at the top
+# Add the "published" text at the bottom
 st.sidebar.write("Published 07/07/2023")
-# st.sidebar.text("published 07/07/2023")
 
-
-
-            
-st.title('🏀 NBA GPT | 2022-2023 NBA Season')
+# Text above prompt bar.
 prompt = st.text_input('Type in your prompt here')
 
+# This is the query that chat-gpt will answer. Custom prompt + whatever the user types in as {prompt}.
 query = "Give me ALL the info you know about this. Even if you don't know much, expand on what you do know and go in depth:" + prompt
 
-
-#downloads_path = os.path.expanduser("~/Desktop/dataVU")
-# Create a DirectoryLoader with the Downloads folder and "*.txt" as the glob pattern
-#loader = DirectoryLoader(downloads_path, glob="*.json")
-
-
+# Create the llm and load data from .txt file.
 llm = OpenAI(temperature=0.9)
 loader = TextLoader('data.txt')
 index = VectorstoreIndexCreator().from_loaders([loader])
 
+# To pass in more than one file:
+    # loader = DirectoryLoader('.', glob="x.txt")
 
-#print(index.query(query, llm=ChatOpenAI()))
+# Given a prompt, pass our query into the llm.
 if prompt:
-    #response = llm(prompt)
     st.write(index.query(query, llm=ChatOpenAI()))
-
-# #loader = DirectoryLoader('.', glob="x.txt")
-
-
-
-
--- getting_data.py
+++ getting_data.py
@@ -1,92 +1,52 @@
 import os
 import sys
 import math
-import constants
-import time
 import pandas as pd
 import requests
 from nba_api.stats.static import players
 from nba_api.stats.endpoints import commonplayerinfo
 from nba_api.stats.endpoints import playercareerstats
 
+# Get the NBA players from the API and create data-frames.
 nba_players = players.get_players()
 df = pd.DataFrame(nba_players)
 
-
-# List of only active players
+# List of only active players -- 2 ways to do the same thing.
 active_player_ids = df.loc[df['is_active'], 'id'].tolist()
-
-
 active_players = players.get_active_players()
 
-
-#print(active_players[0]['full_name'])
-#{'id': 1630173, 'full_name': 'Precious Achiuwa', 'first_name': 'Precious', 'last_name': 'Achiuwa', 'is_active': True}
-#print(sliced_data)
-
-
-# THIS GETS ALL PLAYER INFORMATION
+# This function gets the data of any player given the ID.
 def get_player_data(nba_player_id):
     player_info = commonplayerinfo.CommonPlayerInfo(player_id=nba_player_id, timeout=100)
     df = player_info.common_player_info.get_data_frame()
     return df
 
-
-#all_player_data = []
-
-
+# This is all to get the NBA API more efficently. Split and extract the data in fourths, rather than all at once (was taking to long).
 player_career_data = []
-
-
-#new
 num_players = len(active_player_ids)
 num_fourths = 4
 players_per_fourth = math.ceil(num_players / num_fourths)
 
-
 # Create four different arrays for each fourth of active players
 fourths = [active_player_ids[i:i+players_per_fourth] for i in range(0, num_players, players_per_fourth)]
-
 
 # Access each fourth array individually
 fourth_1 = fourths[0]
 fourth_2 = fourths[1]
 fourth_3 = fourths[2]
 fourth_4 = fourths[3]
-#end
-
-
-time.sleep(.600)
 
 for nba_player_id in fourth_1:
-# player_info = get_player_data(nba_player_id)
-# all_player_data.append(player_info)
     career = playercareerstats.PlayerCareerStats(player_id = nba_player_id, timeout=100)
     temp = career.get_data_frames()[0]
     sliced = temp.iloc[:, [1] + list(range(3, temp.shape[1]))]
-
     player_career_data.append(sliced)
+# End data extraction.
 
-
+# Writing the data to a .txt file.
 file_path = "data.txt"
 
-print(len(active_players))
-print(len(player_career_data))
-
-
-# Write the player career data to the .txt file
-time.sleep(.600)
 with open(file_path, 'w') as file:
     for i, j in zip(range(0, 134), range(0, 134)):
-# file.write(str(active_players[i]) + '\n')
-# keys_to_exclude = ['id', 'full_name', 'first_name'] # Specify the keys to exclude
-
-
-# sliced_data = [{key: player[key] for key in player.keys() if key not in keys_to_exclude} for player in active_players]
         file.write(str(active_players[i]['full_name']) + '\n' + str((player_career_data[j])) + '\n')
-# file.write(data_final.to_string(index=False) + '\n')
-# file.write(data_df.to_string(index=False) + '\n')
-
-
-# career = playercareerstats.PlayerCareerStats(nbaPLAYER_id)
-# df = career.get_data_frames()[0]
+# End writing --> .txt file.
